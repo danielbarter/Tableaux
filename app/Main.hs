7aaeb5cea73da6f9c795984af2efaf9a743201db
danielbarter <danielbarter@gmail.com>
2015-11-15 18:54:44 +0000

    improved front end

--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,27 @@ module Main where
 import ContentVector (isDecreasing)
 import GameState
 import Control.Monad.State.Lazy
+import Text.Read (readMaybe)
+
+parsePartition :: IO [Int]
+parsePartition = do putStr "choose a weakly decreasing sequence like [3,3,2,1] : "
+                    text <- getLine
+                    let p = (readMaybe text) :: Maybe [Int]
+                    case p of 
+                      Just l  -> if isDecreasing l then putStrLn "the game begins!" >> return l
+                                                   else putStrLn "your sequence was not weakly decreasing" >> parsePartition
+                      Nothing -> putStrLn "sorry, I didn't catch that" >> parsePartition 
+
+parseTransformation :: IO Int
+parseTransformation = do putStr "transformation : "
+                         text <- getLine
+                         let x = (readMaybe text) :: Maybe Int
+                         case x of Just m  -> return m
+                                   Nothing -> putStrLn "sorry, I didn't catch that" >> parseTransformation
 
 main :: IO ()
-main = do putStr "choose a weakly decreasing sequence like [3,3,2,1] : "
-          text <- getLine
-          let p = (read text) :: [Int]
-          if isDecreasing p then do putStrLn $ "the game begins" 
-                                    enterGame p
-                            else putStrLn "your sequence was not weakly decreasing" >> return ()
-
+main = do p <- parsePartition
+          enterGame p
 
 enterGame :: [Int] -> IO ()
 enterGame p = do state <- execStateT (gameOver) (initialState p)
@@ -22,9 +34,7 @@ enterGame p = do state <- execStateT (gameOver) (initialState p)
 playGame :: GameState -> IO ()
 playGame state = do let GameState e t f n b = state
                     if b then return ()
-                         else do putStr "transformation : "
-                                 text <- getLine
-                                 let x = (read text) :: Int
+                         else do x <- parseTransformation
                                  newState <- execStateT (transform x >> gameOver) state
                                  putStrLn $ show newState
                                  playGame newState
